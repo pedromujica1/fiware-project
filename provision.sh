5ce00128550db6f53852a6157c3e3d520f5af88b
pedromujica1 <miottopedro@gmail.com>
2024-10-17 16:48:33 -0300

    changes lora

---
+++ provision.sh
@@ -0,0 +1,195 @@
+#!/bin/sh
+
+
+curl --location --request POST 'http://localhost:4041/iot/devices' \
+--header 'fiware-service: smartgondor' \
+--header 'fiware-servicepath: /gardens' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "devices": [
+        {
+            "device_id": "env1",
+            "entity_name": "SensorQualidadeAr_Londrina",
+            "entity_type": "LoraDevice",
+            "attributes": [
+                {
+                    "object_id": "best_co",
+                    "name": "Best_CO",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "best_no2",
+                    "name": "Best_NO2",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "best_ox",
+                    "name": "Best_OX",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "best_so2",
+                    "name": "Best_SO2",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "co_1",
+                    "name": "CO_1",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "co_2",
+                    "name": "CO_2",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "co_3",
+                    "name": "CO_3",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "co_4",
+                    "name": "CO_4",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "co_ae",
+                    "name": "CO_AE",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "co_we",
+                    "name": "CO_WE",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "no2_1",
+                    "name": "NO2_1",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "no2_2",
+                    "name": "NO2_2",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "no2_3",
+                    "name": "NO2_3",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "no2_4",
+                    "name": "NO2_4",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "no2_ae",
+                    "name": "NO2_AE",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "no2_we",
+                    "name": "NO2_WE",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "ox_1",
+                    "name": "OX_1",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "ox_2",
+                    "name": "OX_2",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "ox_3",
+                    "name": "OX_3",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "ox_4",
+                    "name": "OX_4",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "ox_ae",
+                    "name": "OX_AE",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "ox_we",
+                    "name": "OX_WE",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "so2_1",
+                    "name": "SO2_1",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "so2_2",
+                    "name": "SO2_2",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "so2_3",
+                    "name": "SO2_3",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "so2_4",
+                    "name": "SO2_4",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "so2_ae",
+                    "name": "SO2_AE",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "so2_we",
+                    "name": "SO2_WE",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "temperatura_1",
+                    "name": "Temperatura_1",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "umidade_1",
+                    "name": "Umidade_1",
+                    "type": "Float"
+                },
+                {
+                    "object_id": "data",
+                    "name": "DATA",
+                    "type": "Date"
+                },
+                {
+                    "object_id": "hora",
+                    "name": "HORA",
+                    "type": "Text"
+                }
+            ],
+            "internal_attributes": {
+                "lorawan": {
+                    "application_server": {
+                        "host": "au1.cloud.thethings.network",
+                        "username": "envcity-aqm@ttn",
+                        "password": "NNSXS.MEBFVHICSHLPKAUOXL2I5RLMZILIFM6PWTSIHOY.UD7ICI2IIMKG7F6KYKJPQQWUTE4ZPB6XFU5KHWM3PZQEDSP4JVLA",
+                        "provider": "TTS-V3"
+                    },
+                    "app_eui": "1231231231231231",
+                    "dev_eui": "70B3D57ED00678EB",
+                    "application_id": "envcity-aqm@ttn",
+                    "application_key": "E078A5F764CFA112E6BA26496CA19A5B",
+                    "data_model": "application_server"
+                }
+            }
+        }
+    ]
+}'
+
+
---
+++ provision2.sh
@@ -0,0 +1,55 @@
+
+curl localhost:4041/iot/devices -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'fiware-service: smartgondor' --header 'fiware-servicepath: /gardens' -d @- <<EOF
+{
+  "devices": [
+    {
+      "device_id": "lora_n_003",
+      "entity_name": "LORA-N-003",
+      "entity_type": "LoraDevice",
+      "timezone": "America/Santiago",
+      "attributes": [
+        {
+          "object_id": "bp0",
+          "name": "barometric_pressure_0",
+          "type": "hpa"
+        },
+        {
+          "object_id": "di3",
+          "name": "digital_in_3",
+          "type": "Number"
+        },
+        {
+          "object_id": "do4",
+          "name": "digital_out_4",
+          "type": "Number"
+        },
+        {
+          "object_id": "rh2",
+          "name": "relative_humidity_2",
+          "type": "Number"
+        },
+        {
+          "object_id": "t1",
+          "name": "temperature_1",
+          "type": "Number"
+        }
+      ],
+      "internal_attributes": {
+        "lorawan": {
+          "application_server": {
+            "host": "eu.thethings.network",
+            "username": "ari_ioe_app_demo1",
+            "password": "pwd1",
+            "provider": "TTN"
+          },
+          "dev_eui": "1119343755556A14",
+          "app_eui": "4569343567897875",
+          "application_id": "ari_ioe_app_demo1",
+          "application_key": "444B8EF16415B5F6ED777EAFE695C49",
+          "data_model": "cayennelpp"
+        }
+      }
+    }
+  ]
+}
+EOF
