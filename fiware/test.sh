8f53a32209441bf19d5a425938d1c607eb43af9a
pedromujica1 <miottopedro@gmail.com>
2024-11-04 12:56:33 -0300

    fazendo commit

--- fiware/test.sh
+++
@@ -1,2 +0,0 @@
-echo "envcity-aqm@ttn" | docker secret create mqtt_username -
-echo "NNSXS.MEBFVHICSHLPKAUOXL2I5RLMZILIFM6PWTSIHOY.UD7ICI2IIMKG7F6KYKJPQQWUTE4ZPB6XFU5KHWM3PZQEDSP4JVLA" | docker secret create mqtt_password -
