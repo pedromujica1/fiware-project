dceecc5489bacbb6f76a1d81a650befdcecce93b
pedro.mujica1 <miottopedro@gmail.com>
2024-11-20 19:04:12 -0300

    Draco integration sucessfull

--- provision.sh
+++ provision.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 curl --location --request POST 'http://localhost:4041/iot/devices' \
 --header 'fiware-service: openiot' \
 --header 'fiware-servicepath: /airQuality' \
@@ -11,166 +12,38 @@ curl --location --request POST 'http://localhost:4041/iot/devices' \
             "entity_name": "SensorQualidadeAr_Londrina",
             "entity_type": "LoraDevice",
             "attributes": [
-                {
-                    "object_id": "best_co",
-                    "name": "Best_CO",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "best_no2",
-                    "name": "Best_NO2",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "best_ox",
-                    "name": "Best_OX",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "best_so2",
-                    "name": "Best_SO2",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "co_1",
-                    "name": "CO_1",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "co_2",
-                    "name": "CO_2",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "co_3",
-                    "name": "CO_3",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "co_4",
-                    "name": "CO_4",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "co_ae",
-                    "name": "CO_AE",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "co_we",
-                    "name": "CO_WE",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "no2_1",
-                    "name": "NO2_1",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "no2_2",
-                    "name": "NO2_2",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "no2_3",
-                    "name": "NO2_3",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "no2_4",
-                    "name": "NO2_4",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "no2_ae",
-                    "name": "NO2_AE",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "no2_we",
-                    "name": "NO2_WE",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "ox_1",
-                    "name": "OX_1",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "ox_2",
-                    "name": "OX_2",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "ox_3",
-                    "name": "OX_3",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "ox_4",
-                    "name": "OX_4",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "ox_ae",
-                    "name": "OX_AE",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "ox_we",
-                    "name": "OX_WE",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "so2_1",
-                    "name": "SO2_1",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "so2_2",
-                    "name": "SO2_2",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "so2_3",
-                    "name": "SO2_3",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "so2_4",
-                    "name": "SO2_4",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "so2_ae",
-                    "name": "SO2_AE",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "so2_we",
-                    "name": "SO2_WE",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "temperatura_1",
-                    "name": "Temperatura_1",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "umidade_1",
-                    "name": "Umidade_1",
-                    "type": "Float"
-                },
-                {
-                    "object_id": "data",
-                    "name": "DATA",
-                    "type": "Date"
-                },
-                {
-                    "object_id": "hora",
-                    "name": "HORA",
-                    "type": "Text"
-                }
+                { "object_id": "best_co", "name": "Best_CO", "type": "Float"},
+                { "object_id": "best_no2", "name": "Best_NO2", "type": "Float"},
+                { "object_id": "best_ox", "name": "Best_OX", "type": "Float"},
+                { "object_id": "best_so2", "name": "Best_SO2", "type": "Float"},
+                { "object_id": "co_1", "name": "CO_1", "type": "Float"},
+                { "object_id": "co_2", "name": "CO_2", "type": "Float"},
+                { "object_id": "co_3", "name": "CO_3", "type": "Float"},
+                { "object_id": "co_4", "name": "CO_4", "type": "Float"},
+                { "object_id": "co_ae", "name": "CO_AE", "type": "Float"},
+                { "object_id": "co_we", "name": "CO_WE", "type": "Float"},
+                { "object_id": "no2_1", "name": "NO2_1", "type": "Float"},
+                { "object_id": "no2_2", "name": "NO2_2", "type": "Float"},
+                { "object_id": "no2_3", "name": "NO2_3", "type": "Float"},
+                { "object_id": "no2_4", "name": "NO2_4", "type": "Float"},
+                { "object_id": "no2_ae", "name": "NO2_AE", "type": "Float"},
+                { "object_id": "no2_we", "name": "NO2_WE", "type": "Float"},
+                { "object_id": "ox_1", "name": "OX_1", "type": "Float"},
+                { "object_id": "ox_2", "name": "OX_2", "type": "Float"},
+                { "object_id": "ox_3", "name": "OX_3", "type": "Float"},
+                { "object_id": "ox_4", "name": "OX_4", "type": "Float"},
+                { "object_id": "ox_ae", "name": "OX_AE", "type": "Float"},
+                { "object_id": "ox_we", "name": "OX_WE", "type": "Float"},
+                { "object_id": "so2_1", "name": "SO2_1", "type": "Float"},
+                { "object_id": "so2_2", "name": "SO2_2", "type": "Float"},
+                { "object_id": "so2_3", "name": "SO2_3", "type": "Float"},
+                { "object_id": "so2_4", "name": "SO2_4", "type": "Float"},
+                { "object_id": "so2_ae", "name": "SO2_AE", "type": "Float"},
+                { "object_id": "so2_we", "name": "SO2_WE", "type": "Float"},
+                { "object_id": "temperatura_1", "name": "Temperatura_1", "type": "Float"},
+                { "object_id": "umidade_1", "name": "Umidade_1", "type": "Float"},
+                { "object_id": "data", "name": "DATA", "type": "Text"},
+                { "object_id": "hora", "name": "HORA", "type": "Text"}
             ],
             "internal_attributes": {
                 "lorawan": {
@@ -190,5 +63,3 @@ curl --location --request POST 'http://localhost:4041/iot/devices' \
         }
     ]
 }'
-
-
