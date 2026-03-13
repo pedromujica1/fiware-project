23258946bc94c574e48f2ef5d5d5602167a8b6fd
pedro.mujica1 <miottopedro@gmail.com>
2026-03-13 15:59:38 -0300

    RESOLVEU FUCKING A DUPLICAÇÃO

--- dummy/stop.sh
+++ dummy/stop.sh
@@ -1,12 +1,51 @@
 ##!/usr/bin/env bash
 
 curl --location --request DELETE 'localhost:4041/iot/services?resource=70B3D57ED00006B2&apikey=' \
---header 'fiware-service: smartgondor' \
---header 'fiware-servicepath: /environment'
+--header 'fiware-service: openiot' \
+--header 'fiware-servicepath: /airQuality'
 
 
 curl --location --request DELETE 'localhost:4041/iot/devices/myDevice' \
---header 'fiware-service: smartgondor' \
---header 'fiware-servicepath: /environment'
+--header 'fiware-service: openiot' \
+--header 'fiware-servicepath: /airQuality'
 
 docker compose down -v
+
+curl --location 'http://localhost:1026/v2/subscriptions' \
+--header 'Content-Type: application/json' \
+--header 'fiware-service: openiot' \
+--header 'fiware-servicepath: /airQuality' \
+--data '{
+  "description": "Notify Cygnus only sensor attrs",
+  "subject": {
+    "entities": [
+      {
+        "idPattern": ".*",
+        "type": "WeatherObserved"
+      }
+    ],
+    "condition": {
+      "attrs": [
+        "temperature",
+        "pressure",
+        "relative_humidity"
+      ]
+    }
+  },
+  "notification": {
+    "http": {
+      "url": "http://cygnus:5055/notify"
+    },
+    "attrs": [
+      "temperature",
+      "pressure",
+      "relative_humidity"
+    ],
+    "onlyChangedAttrs": true
+  },
+  "throttling": 0
+}'
+
+curl -X DELETE \ http://localhost:1026/v2/subscriptions/65d8a3f3c0a9e3d1f9d12345 \
+-H "Fiware-Service: openiot" \
+-H "Fiware-ServicePath: /airQuality"
--- dummy/test-data.sh
+++ dummy/test-data.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 echo 'sending dummy data to mqtt queue using TTN format'
-docker exec -ti mosquitto mosquitto_pub -h mqtt -u admin -P password -t v3/demoTTN/devices/myDevice0/up -m '{
+docker exec -ti mosquitto mosquitto_pub -h mqtt -u admin -P password -t v3/demoTTN/devices/myDevice/up -m '{
   "app_id": "demoTTN",
-  "dev_id": "myDevice0",
+  "dev_id": "myDevice",
   "hardware_serial": "0102030405060708",
   "port": 1,
-  "counter": 2,
+  "counter": 1,
   "is_retry": false,
   "confirmed": false,
   "payload_raw": "AHMnSwFnARYCaFADAGQEAQAFAdc="
 }'
 
+
 sleep 5
 
 echo 'read sent data from orion'
 
-curl --location --request GET 'http://localhost:1026/v2/entities/urn:WeatherObserved:myDevice0' \
+curl --location --request GET 'http://localhost:1026/v2/entities/urn:WeatherObserved:myDevice' \
 --header 'fiware-service: openiot' \
 --header 'fiware-servicePath:  /airQuality'  | jq
